079785a601665fdbf44eee0604f7ce259d21a19c
ThomasGeorge <t6george@edu.uwaterloo.ca>
2020-12-26 01:58:58 -0500

    a4 cleared

--- machine-learning-ex/ex4/nnCostFunction.m
+++ machine-learning-ex/ex4/nnCostFunction.m
@@ -62,23 +62,49 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+o = ones(m, 1);
 
+z2 = [o, X] * Theta1';
+a2 = [o, sigmoid(z2)];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
+logh = log(a3);
+log1mh = log(1-a3);
+one_hot = zeros(num_labels, 1);
 
+for i = 1:m
+    one_hot(y(i)) = 1;
+    J = J + logh(i,:) * one_hot + log1mh(i,:) * (1 - one_hot);    
+    one_hot(y(i)) = 0;
+end
 
+J = J / (-m);
 
+J = J + lambda * (sum((Theta1(:,2:end) .^ 2),'all') + sum((Theta2(:,2:end) .^ 2), 'all')) / (2 * m);
 
+% Gradient Calculation (Backpropagation)
+sigGrad = sigmoidGradient(z2);
 
+for i = 1:m
+    one_hot(y(i)) = 1;
+    
+    delta3 = a3(i,:)' - one_hot;
+    
+    delta2 = (Theta2' * delta3) .* [1; sigGrad(i,:)'];
+    delta2 = delta2(2:end);
+    
+    Theta1_grad = Theta1_grad + delta2 * [1, X(i,:)];
+    Theta2_grad = Theta2_grad + delta3 * a2(i,:);
+    
+    one_hot(y(i)) = 0;
+end
 
+Theta1_grad = Theta1_grad ./ m;
+Theta2_grad = Theta2_grad ./ m;
 
-
-
-
-
-
-
-
-
+Theta1_grad = Theta1_grad + (lambda / m) * ([zeros(size(Theta1,1),1), Theta1(:, 2:end)]);
+Theta2_grad = Theta2_grad + (lambda / m) * ([zeros(size(Theta2,1),1), Theta2(:, 2:end)]);
 
 % -------------------------------------------------------------
 
@@ -89,3 +115,4 @@ grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 
 end
+
